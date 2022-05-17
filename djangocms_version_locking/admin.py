1c39c6234fb1f452282a268c39b9cd869905eb98
Adam Murray <adam-murray@hotmail.co.uk>
2022-05-17 09:59:11 +0100

    Feat: Added get_name method

--- djangocms_version_locking/admin.py
+++ djangocms_version_locking/admin.py
@@ -1,4 +1,10 @@
+from django.template.loader import render_to_string
+from django.utils.html import format_html
+
+from djangocms_versioning.constants import DRAFT
+
 from djangocms_versioning.admin import VersioningAdminMixin
+from djangocms_version_locking.helpers import version_is_locked
 
 
 class VersionLockAdminMixin(VersioningAdminMixin):
@@ -6,12 +12,25 @@ class VersionLockAdminMixin(VersioningAdminMixin):
     Mixin providing versioning functionality to admin classes of
     version models.
     """
-    lock_display_field = "name"
 
     def get_list_display(self, request):
         return (
+            "get_name",
             "nr",
         )
+
+    def get_name(self, request, obj):
+        return format_html(
+            "{lock}{name}",
+            lock=self.is_locked(obj),
+            name=getattr(obj, "name", obj.__str__)
+        )
+
+    def is_locked(self, obj):
+        version = self.get_version(obj)
+        if version.state == DRAFT and version_is_locked(version):
+            return render_to_string("djangocms_version_locking/admin/locked_icon.html")
+        return ""
 
     def has_change_permission(self, request, obj=None):
         """
